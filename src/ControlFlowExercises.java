9d58ae12d0537ab6ce94df42fd8d0ba38548ea48
leo-ortiz22 <101903651+leo-ortiz22@users.noreply.github.com>
2022-06-08 16:11:38 -0500

    string exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -10,7 +10,7 @@ public class ControlFlowExercises {
 //        int i = 5;
 //
 //        while (i <= 15) {
-//            System.out.println(i);
+//            System.out.print(i + " ");
 //            i = i + 1;
 //        }
 
@@ -22,7 +22,7 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        } while (i <= 1000000);
 
-
+        //C.
 //        int i;
 //        for (i = 5; i < 15; i++ ){
 //            System.out.println(i);
@@ -39,11 +39,11 @@ public class ControlFlowExercises {
         //Question 2.
 
 //        for (int i = 1; i <= 100; i++)
-//            if (((i % 3) == 0) && ((i % 5) == 0))
+//            if (i % 3 == 0 && i % 5 == 0)
 //                System.out.println("fizzbuzz");
-//            else if ((i % 3) == 0)
+//            else if (i % 3 == 0)
 //                System.out.println("fizz");
-//            else if ((i % 5) == 0)
+//            else if (i % 5 == 0)
 //                System.out.println("buzz");
 //            else
 //                System.out.println(i);
@@ -62,27 +62,43 @@ public class ControlFlowExercises {
 
         } System.out.println(userInput + "     |");*/
 
+        boolean willContinue = false;
 
-        System.out.println("What number would you like to go up to? ");
-        String limit = scanner.nextLine();                              // user input is the limit - string
-        int limitNum = Integer.parseInt(limit);                         // turn limit into a number
-        System.out.println();                                           // line return for spacing
-        System.out.println("Here is your table");                       // begin table header info
-        System.out.println();
-        System.out.println("number | squared | cubed");
-        System.out.println("------ | ------- | -----");
-        for (int i = 1; i <= limitNum; i++) {                           // for loop to print out table body
-            int number = i;
-            int squared = i * i;
-            int cubed = i * i * i;
-            System.out.format("%-6d | %-7d | %-5d%n", number, squared, cubed);  // This took the longest to develop
+        // Help for number 3 From Chris lee
+//do {
+//    System.out.println("What number would you like to go up to? ");
+//    String limit = scanner.nextLine();                              // user input is the limit - string
+//    int limitNum = Integer.parseInt(limit);                         // turn limit into a number
+//    System.out.println();                                           // line return for spacing
+//    System.out.println("Here is your table");                       // begin table header info
+//    System.out.println();
+//    System.out.println("number | squared | cubed");
+//    System.out.println("------ | ------- | -----");
+//    for (int i = 1; i <= limitNum; i++) {
+//        int number = i;
+//        int squared = i * i;
+//        int cubed = i * i * i;
+//        System.out.format("%-6d | %-7d | %-5d%n", number, squared, cubed);
+//    }
+//    System.out.println("Do you want to enter another number? (y/n)");
+//    String userResponse = scanner.next();
+//    if (userResponse.equalsIgnoreCase("y")) {
+//        willContinue = (true);
+//    } else {
+//        willContinue = (false);
+//    }
+//}while (willContinue);
+//
 
 
 //Question 4.
+
+
 //        System.out.println("Enter a grade value from 0 to 100");
 //        int userGrade = scanner.nextInt();
 //        System.out.println("Would you like to see your grade?  Please enter Yes or No");
 //        String userContinue = scanner.next();
+
 //        if (userContinue.equals("yes")) {
 //            if (userGrade > 87) {
 //                System.out.println("Your grade is an A");
@@ -98,6 +114,6 @@ public class ControlFlowExercises {
 //        } else {
 //            System.out.println("No problem!");
 //        }
-        }
+
     }
 }
